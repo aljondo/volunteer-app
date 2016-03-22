c9ef4b2eca7c46a285702ac46d077d2c5477bfdd
Branden Rodgers <branden@rodgersworld.com>
2016-03-22 19:22:09 -0400

    fixed page routing
    removed commented out basename code

--- app/views/App.js
+++ app/views/App.js
@@ -1,1 +1,24 @@
 //Will be a view wrapper if any compents which are the same through out the application.
+import React, { Component } from 'react';
+import {Link} from 'react-router'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Americorps
+                <header>
+                    {' '}
+                    <Link to="/">Home</Link>
+                    {' '}
+                    <Link to="profile">Profile</Link>
+                    {' '}
+                    <Link to="search">Search</Link>
+                </header>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+export default App
