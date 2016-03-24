e3d66c9dd7713e82aa2048866fdd43efb88c81d1
Zach Ferland <zachferland@gmail.com>
2016-03-24 19:00:41 -0400

    connect api routes

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,11 +1,14 @@
 import { combineReducers } from 'redux'
 //move to top of the app
 import { routerReducer as router } from 'react-router-redux'
-import users from './users';
+import users from './users'
+import events from './events'
+import orgs from './orgs'
 
 //combine all reducers in this folder here
-
 export default combineReducers({
   router,
-  users
+  users,
+  events,
+  orgs
 })
