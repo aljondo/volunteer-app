ed19c0aa156c4d53b7d55bd96cc9cbd2a86d2eed
Zach Ferland <zachferland@gmail.com>
2016-03-24 18:57:22 -0400

    add api middleware support for HTTP POST method

--- app/middleware/api.js
+++ app/middleware/api.js
@@ -3,22 +3,47 @@ import 'isomorphic-fetch'
 //move to dev enviroment and set production in env vars
 const API_ROOT = 'http://localhost:3004/'
 
+//Supported HTTP Methods
+export const GET = 'GET'
+export const POST = 'POST'
+
+// A default json http post header
+const jsonHttpHeader = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+
 // Fetches an API response
-function callApi(endpoint) {
+function callApi(endpoint, method, data) {
   const fullUrl = API_ROOT + endpoint
 
-  return fetch(fullUrl)
-    .then(response =>
-      response.json().then(json => ({ json, response }))
-    ).then(({ json, response }) => {
-      if (!response.ok) {
-        return Promise.reject(json)
-      }
-
-      return json
-      // may want to wrap in object
-      // return Object.assign({}, json)
-    })
+  switch (method) {
+    case GET:
+      return fetch(fullUrl)
+        .then(response =>
+          response.json().then(json => ({ json, response }))
+        ).then(({ json, response }) => {
+          if (!response.ok) {
+            return Promise.reject(json)
+          }
+          return json // may want to wrap in object
+          // return Object.assign({}, json)
+        })
+    case POST:
+      return fetch(fullUrl, {
+          method: 'post',
+          headers: jsonHttpHeader,
+          body: JSON.stringify(data)
+        })
+        .then(response =>
+          response.json().then(json => ({ json, response }))
+        ).then(({ json, response }) => {
+          if (!response.ok) {
+            return Promise.reject(json)
+          }
+          return json // may want to wrap in object
+          // return Object.assign({}, json)
+        })
+    default:
+      throw new Error('Unsupported HTTP method')
+  }
 }
 
 // may want to use Normalizr: https://github.com/gaearon/normalizr if  we
@@ -30,7 +55,9 @@ export const CALL_API = Symbol('Call API')
 // actions of the following of form:
 // [CALL_API]: {
 //   types: [ OBJECT_REQUEST, OBJECT_SUCCESS, OBJECT_FAILURE ],
-//   endpoint: `object`
+//   endpoint: `object`,
+//   method: POST
+//   data: json_obj    // only if a post request
 // }
 
 export default store => next => action => {
@@ -42,7 +69,7 @@ export default store => next => action => {
   }
 
   let { endpoint } = callAPI
-  const { types } = callAPI
+  const { types, method, data } = callAPI
 
   // check args
   if (typeof endpoint !== 'string') {
@@ -52,8 +79,13 @@ export default store => next => action => {
   if (!Array.isArray(types) || types.length !== 3) {
     throw new Error('Expected an array of three action types.')
   }
+
   if (!types.every(type => typeof type === 'string')) {
     throw new Error('Expected action types to be strings.')
+  }
+
+  if (typeof method !== 'string') {
+    throw new Error("Specify a HTTP method.")
   }
 
   function actionWith(data) {
@@ -65,7 +97,7 @@ export default store => next => action => {
   const [ requestType, successType, failureType ] = types
   next(actionWith({ type: requestType }))
 
-  return callApi(endpoint).then(
+  return callApi(endpoint, method, data).then(
     response => next(actionWith({
       response,
       type: successType
