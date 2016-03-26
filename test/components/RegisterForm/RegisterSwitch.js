ae9815247ce877102f45b225caad797cac605b46
Branden Rodgers <branden@rodgersworld.com>
2016-03-25 22:01:51 -0400

    initial work for register pages

---
+++ test/components/RegisterForm/RegisterSwitch.js
@@ -0,0 +1,25 @@
+// add paths from route not relative
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RegisterSwitch from '../../../app/components/RegisterForm/RegisterSwitch';
+import { assert }  from "chai";
+import TestUtils from "react-addons-test-utils"
+
+
+describe('RegisterSwitch Component', () => {
+
+    function fakeMethod(){
+        // IDK why this fixes it, but oh well
+    }
+
+    const renderedComponent = TestUtils.renderIntoDocument(
+        <RegisterSwitch onSelection={fakeMethod}/>
+    );
+
+    const dom = ReactDOM.findDOMNode(renderedComponent);
+
+
+    it('it renders correctly', () => {
+        return assert.isOk(renderedComponent);
+    });
+});
---
+++ test/components/RegisterForm/UserRegisterForm.js
@@ -0,0 +1,20 @@
+// add paths from route not relative
+import React from 'react';
+import ReactDOM from 'react-dom';
+import UserRegisterForm from '../../../app/components/RegisterForm/UserRegisterForm';
+import { assert }  from "chai";
+import TestUtils from "react-addons-test-utils"
+
+
+describe('UserRegisterForm Component', () => {
+    const renderedComponent = TestUtils.renderIntoDocument(
+        <UserRegisterForm />
+    );
+
+    const dom = ReactDOM.findDOMNode(renderedComponent);
+
+
+    it('it renders correctly', () => {
+        return assert.isOk(renderedComponent);
+    });
+});
