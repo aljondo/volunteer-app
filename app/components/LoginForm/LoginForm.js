ef05473b7bb390128f918252e2a9cba15966ac23
Zach Ferland <zachferland@gmail.com>
2016-04-01 23:04:09 -0400

    add login logic in a login form container

--- app/components/LoginForm/LoginForm.js
+++ app/components/LoginForm/LoginForm.js
@@ -3,28 +3,24 @@ import { Grid, Col, Row, Input, Button  } from 'react-bootstrap'
 import styles from './loginForm.scss'
 
 // a Login Form component
-class LoginForm extends Component {
-    render() {
-        return (
+const LoginForm = (props) => (
             <Grid>
                 <Row className={styles.form}>
                     <Col xs={10} xsOffset={1}>
                         <form className="form-horizontal" >
                             <Row>
-                                <Input type="email" id="email" label="Email:" placeholder="Enter Email"/>
-                                <Input type="password" id="password" label="Password:" placeholder="Enter Password" />
+                                <Input type="email" id="email" onChange={props.emailChange} label="Email:" placeholder="Enter Email"/>
+                                <Input type="password" id="password" onChange={props.passwordChange} label="Password:" placeholder="Enter Password" />
                             </Row>
                             <Row className="form-group">
                                 <Col sm={12} className="text-center">
-                                    <Button bsSize="large" bsStyle="primary" type="submit">Login</Button>
+                                    <Button bsSize="large" bsStyle="primary" onClick={props.handleLogin}>Login</Button>
                                 </Col>
                             </Row>
                         </form>
                     </Col>
                 </Row>
             </Grid>
-        );
-    }
-}
+)
 
 export default LoginForm
---
+++ app/containers/LoginFormContainer.js
@@ -0,0 +1,59 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { loginRequest,
+         setEmail,
+         setPassword } from '../actions/auth'
+import { push as pushRoute} from 'react-router-redux';
+import LoginForm from '../components/LoginForm/LoginForm'
+
+
+class LoginFormContainer extends Component {
+
+  componentWillUpdate(nextProps, nextState) {
+    if (nextProps.isAuthenticated) {
+      nextProps.pushRoute('/profile');
+    }
+  }
+
+  handleLogin() {
+    this.props.loginRequest({email: this.props.email, password: this.props.password})
+  }
+
+  emailChange(e) {
+    this.props.setEmail(e.target.value)
+  }
+
+  passwordChange(e) {
+    this.props.setPassword(e.target.value)
+  }
+
+  render() {
+      return (
+        <LoginForm handleLogin={this.handleLogin.bind(this)}
+                   passwordChange={this.passwordChange.bind(this)}
+                   emailChange={this.emailChange.bind(this)}/>
+      );
+  }
+}
+
+LoginFormContainer.propTypes = {
+  email: PropTypes.string,
+  password: PropTypes.string,
+  error: PropTypes.string
+}
+
+const mapDispatchToProps = (dispatch) => (
+    bindActionCreators({pushRoute, loginRequest, setEmail, setPassword }, dispatch)
+)
+
+const mapStateToProps = (state) => (
+  { email: state.auth.user.email,
+    password: state.auth.user.password,
+    error: state.auth.error,
+    isFetching: state.auth.isFetching,
+    isAuthenticated: state.auth.isAuthenticated,
+  }
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginFormContainer)
