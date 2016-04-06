0d1939e1135b0d3c3ca073de5d72314e36ad4c19
Shivi Singh <shivi.singh94@yahoo.com>
2016-04-05 21:12:41 -0400

    Carousel plus code updates to ES6
    :wq

--- app/components/VolunteerApproval/RequestingAttend.js
+++ app/components/VolunteerApproval/RequestingAttend.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
 import { Button, ButtonToolbar, Input, OverlayTrigger, Popover} from 'react-bootstrap';
 
-var AttendPopover =
-    <Popover id="volconfirm" title="Volunteer Confirmation">
-        <div>
-            <h3> Profile Info + Skills </h3>
-            <h5> PULL FROM PROFILE HERE </h5>
-            <br/>
-            <h3> Volunteer History </h3>
-            <h5> PULL FROM PROFILE HERE </h5>
-            <Button> Confirm </Button>
-        </div>
-    </Popover>
 
+const RequestingAttend = (props) => {
 
-class RequestingAttend extends Component {
+    const AttendPopover =
+        <Popover id="volconfirm" title="Volunteer Confirmation">
+            <div>
+                <h3> Profile Info + Skills </h3>
+                <h5> {props.volunteer.name}</h5>
+                <br/>
+                <h3> Volunteer History </h3>
+                <Button> Confirm </Button>
+            </div>
+        </Popover>
 
-    render() {
-        return (
-            <OverlayTrigger trigger='click' placement='bottom' overlay={AttendPopover}>
-                <img src='http://placekitten.com/g/400/200' />
-            </OverlayTrigger>
-        );
-    }
+        console.log(props);
+            console.log("hey");
+    return (
+        <OverlayTrigger trigger='click' placement='bottom' overlay={AttendPopover}>
+            <img src='http://placekitten.com/g/400/200' />
+        </OverlayTrigger>
+    );
 }
 
 export default RequestingAttend
--- app/views/approval/ApprovalView.js
+++ app/views/approval/ApprovalView.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import VolunteerApproval from '../../components/VolunteerApproval/VolunteerApproval'
+import './approview.css';
+import VolunteerApprovalContainer from '../../containers/VolunteerApprovalContainer/VolunteerApprovalContainer';
 import {Link} from 'react-router';
 
 class ApprovalView extends Component {
   render() {
       return (
           <div id="outer">
-            <VolunteerApproval />
+          	<div id= "volunteer">
+          	    <h1> Volunteer Approval </h1> 
+            	<VolunteerApprovalContainer />
+            </div> 
+            <div id ="hours">
+            <h1> Hour Approval </h1> 
+           		HOUR APPROVAL GOES HERE
+            </div>
           </div>
       );
   }
