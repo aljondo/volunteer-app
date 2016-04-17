34e99f4b11d2adc8c1f0ca7865d4edd0c929900e
Shivi Singh <shivi.singh94@yahoo.com>
2016-04-17 01:15:55 -0400

    index.js to include new reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -8,7 +8,7 @@ import featuredEvents from './events/featuredEventsReducer'
 import orgs from './orgs'
 import registration from './registration'
 import auth from './auth'
-import eventcreation from './events/eventCreation'
+import eventcreation from './events/eventManipulationReducer'
 
 //add an error reducer as well with actions ADD_ERROR, REMOVE_ERROR
 
