349cfd3e4e6ce436f640be5e5cbe4abc75c33bbe
Shivi Singh <shivi.singh94@yahoo.com>
2016-04-17 01:17:21 -0400

    Added the manipulation to event objects

--- app/components/ProfileComponents/Organization/EventDashboard/EventTable.js
+++ app/components/ProfileComponents/Organization/EventDashboard/EventTable.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Table } from 'react-bootstrap';
+import EventManipulationContainer from '../../../../containers/EventManipulationContainer';
 
 const EventTable = (props) => (
     <Table fill responsive bordered={false}>
         <tbody>
         <tr>
             <td> Organization Name </td>
+            <EventManipulationContainer event= {props.event} />
         </tr>
         <tr>
             <td> Time Commitment </td>
