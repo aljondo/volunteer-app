025d7187d05542437dc54283f114d5c58459fc0e
Shivi Singh <shivi.singh94@yahoo.com>
2016-04-19 11:16:13 -0400

    Added twitter feed and featured events on homeview

--- app/views/home/HomeView.js
+++ app/views/home/HomeView.js
@@ -9,9 +9,20 @@ import HomeButtons from '../../components/HomeButtons/HomeButtons'
 import SimpleHeaderContainer from '../../containers/Header/SimpleHeaderContainer'
 import FooterContainer from '../../containers/FooterContainer'
 import styles from './homeview.scss'
+import FeaturedEventsContainer from '../../containers/FeaturedEventsContainer'
 import { Grid, Col, Row, Button  } from 'react-bootstrap'
 import Icon from 'react-fa';
 
+//The twitter feed must be personalized via settings -> widgets in the twitter profile current size @ 400px
+const Twitter = () => (
+
+    <a href="https://twitter.com/your-twitter-page"
+       className="twitter-follow-button"
+       data-show-count="false"
+       data-show-screen-name="false"
+        >
+    </a>
+)
 const HomePage = () => (
   <Row className={styles.homeView}>
     <Row className={styles.sectionOne}>
@@ -45,8 +56,29 @@ const HomePage = () => (
         </Row>
         <Row >
           <Col sm={12} md={6} className={styles.leftpanel}>
+            <div className={styles.holdinfo}>
+              <FeaturedEventsContainer />
+            </div>
           </Col>
           <Col sm={12} md={6} className={styles.rightpanel}>
+              <div className={styles.holdinfo}>
+                  <table className = "table">
+                      <thead>
+                      <tr>
+                          <th>
+                              <h3>Tweet Tweet</h3>
+                          </th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      </tbody>
+                  </table>
+                  <a className="twitter-timeline"
+                     href="https://twitter.com/exampleid2"
+                     data-widget-id="722277701743194112"
+                     data-chrome="transparent noborders nofooter noheader"></a>
+                  <Twitter/>
+              </div>
           </Col>
         </Row>
       </Grid>
