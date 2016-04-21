724e266baed01453ab5c6a257b6dcb5a201609f3
Zach Ferland <zachferland@gmail.com>
2016-04-21 11:07:14 -0400

    add profile page styles

--- app/components/ProfileComponents/Volunteer/UserAboutMe.js
+++ app/components/ProfileComponents/Volunteer/UserAboutMe.js
@@ -1,37 +1,46 @@
 ﻿import React, { Component } from 'react';
 import {Col, Button, Popover, ButtonToolbar, OverlayTrigger, Overlay, Modal, Input} from 'react-bootstrap';
-
-//TODO Go through all available user fields and add them to this. Follow the same general formatting
-//TODO Format this nicer, using scss, but probably wait for Zach to draw up how it should look
+import styles from './useraboutme.scss'
+import Icon from 'react-fa';
 
 const AboutMe = (props) => {
+
     return (
-        <div>
-            <img src={'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSEKgew92pwh3t6IaIj3PazmaN3yia21164I6mvCkFTTr96s5mG'}  alt="user" className="img-responsive"/>
-            <h1>{props.user.name}</h1>
+        <div className={styles.aboutPanel}>
+            <img className={styles.profimg} src={props.user.image}  alt="user"/>
+            <h1 className={styles.name}>{props.user.name}</h1>
             <div>
-                <h2>Profile</h2>
-                <strong>My Bio</strong>
-                <p>{props.user.bio}</p>
-                <strong>Skills</strong>
+
+                <p className={styles.bio}>{props.user.bio}</p>
+                <div className={styles.contact}>
+                  <div className={styles.item}>
+                    <div className={styles.icon}>
+                      <Icon name="phone" size={'2x'} />
+                    </div>
+                    <div className={styles.text}>
+                      {props.user.phone}
+                    </div>
+                  </div>
+                  <div className={styles.item}>
+                    <div className={styles.icon}>
+                      <Icon name="envelope" size={'2x'} />
+                    </div>
+                    <div className={styles.text}>
+                      {props.user.email}
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.skills}>
+                <p className={styles.title}>Skills:</p>
                 <ul>
                     {props.user.skills.map(function(skill, index){
                        return <li key={ index }>{skill}</li>;
                     })}
                 </ul>
-                <p>
-                    <strong>Phone #: </strong>
-                    <span>{props.user.phone}</span>
-                </p>
-                <p>
-                    <strong>Email: </strong>
-                    <span>{props.user.email}</span>
-                </p>
+                </div>
             </div>
-            <br/>
-            <Button bsStyle='primary' onClick={props.openEdit}>Edit</Button>
+            <Button className={styles.edit} bsStyle="primary" onClick={props.openEdit}>Edit Profile</Button>
         </div>
-
     );
 
 };
