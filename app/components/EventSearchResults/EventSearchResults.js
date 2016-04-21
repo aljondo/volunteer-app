b4a4571a9e3fc5bb361b64392ca2127fd4f1fcd2
Branden Rodgers <branden@rodgersworld.com>
2016-04-20 23:13:01 -0400

    search worksgit add .

--- app/components/EventSearchResults/EventSearchResults.js
+++ app/components/EventSearchResults/EventSearchResults.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
 import { Panel, Grid, Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router'
+import Icon from 'react-fa';
 import styles from './searchresult.scss'
 
 
 const EventSearchResults = (props) => (
-    <Col xs={11} className={styles.searchResults}>
+    <Col xs={12}className={styles.searchResults}>
         {props.events.map(function(event, index) {
             return (
-                <div key={index} className={styles.searchResult}>
-                    <Panel collapsible header={event.name} eventKey={index} bsStyle="primary">
-                      {event.name}
-                      <Link to={'/event/' + event.id}>Details</Link>
-                    </Panel>
-                </div>
+                <Col key={index} xs={8} xsOffset={2} className={styles.col}>
+                    <Col xs={12} className={styles.results}>
+                        <Col md={10} xs={12}>
+                            <Col xs={6} className={styles.name}>
+                                {event.name}
+                            </Col>
+                            <Col xs={6} className={styles.date}>
+                                <div className={styles.text}>
+                                    {event.start_at.split(/[ \-\:]+/).splice(0,3).reverse().join("/")}
+                                </div>
+                            </Col>
+                            <Col xs={12} className={styles.about}>
+                                {event.about}
+                            </Col>
+                            <Col xs={12} className={styles.location}>
+                                {event.address} {event.city} {event.state}
+                            </Col>
+                        </Col>
+                        <Col md={2} xs={12} className={styles.detailsBtnCol}>
+                            <Link className={styles.detailsBtnLink}to={"/event/" + event.id}>
+                                <Icon className={styles.detailsBtn} name='arrow-right' size={'3x'}/>
+                            </Link>
+                        </Col>
+                    </Col>
+                </Col>
             );
         })}
     </Col>
