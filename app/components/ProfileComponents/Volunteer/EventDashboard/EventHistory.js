6f5a607791b10f9a51fc040337b12b134b2e7d23
Branden Rodgers <branden@rodgersworld.com>
2016-04-21 13:17:11 -0400

    event dashboard work

--- app/components/ProfileComponents/Volunteer/EventDashboard/EventHistory.js
+++ app/components/ProfileComponents/Volunteer/EventDashboard/EventHistory.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import LogHoursButton from './LogHoursButton'
+import { Panel, Grid, Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router'
+import Icon from 'react-fa';
 import EventTable from './EventTable'
+import styles from './eventhistory.scss'
+import LogHoursButton from './LogHoursButton'
 
 const EventHistory = (props) => (
-
-    <div>
-        <div className="panel panel-primary">
-            <div className="panel-heading">
-                <div className="panel-title pull-left">
-                    <h2>Event Name </h2>
-                </div>
-                <div className='btn-toolbar pull-right'>
-                    <LogHoursButton />
-                </div>
-                <div className='clearfix'> </div>
-            </div>
-            <div className="panel-body">
-                <EventTable />
-            </div>
-        </div>
-    </div>
+    <Col xs={12}className={styles.searchResults}>
+        {props.events.map(function(event, index) {
+            return (
+                <Col key={index} xs={10} xsOffset={1} className={styles.col}>
+                    <Col xs={12} className={styles.results}>
+                        <Col xs={6} className={styles.name}>
+                            {event.name}
+                        </Col>
+                        <Col xs={6} className={styles.detailsBtnCol}>
+                            <LogHoursButton />
+                        </Col>
+                        <Col xs={6} className={styles.location}>
+                            {event.address} {event.city} {event.state}
+                        </Col>
+                        <Col xs={6} className={styles.date}>
+                            <div className={styles.text}>
+                                {event.start_at.split(/[ \-\:]+/).splice(0,3).reverse().join("/")}
+                            </div>
+                        </Col>
+                    </Col>
+                </Col>
+            );
+        })}
+    </Col>
 );
 
 export default EventHistory
